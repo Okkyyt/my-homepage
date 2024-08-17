ce858633ebb0496e7a8044f5b9dfe06b7edaa9d6
Okkyyt <okky.161227@gmail.com>
2024-08-17 23:00:13 +0900

    add mediaquery

--- app/page.tsx
+++ app/page.tsx
@@ -75,20 +75,22 @@ const ProfileCard: React.FC<{ profile: ProfileType }> = ({ profile }) => {
 };
 
 export default function Home() {
-  console.log(SampleSVG);
   const xHandle = process.env.NEXT_PUBLIC_X_URL;
   const githubHandle = process.env.NEXT_PUBLIC_GITHUB_URL;
+
   return (
     <main className="flex flex-col items-center justify-between font-sans w-full overflow-hidden">
       {/* Title */}
-      <div className="text-center mt-20 mb-20 w-full font-bold text-gray-800 relative z-10">
-        <h1 className="text-6xl">Okki-です!!これからよろしく!!</h1>
-        <h2 className="text-4xl mt-4">Python&React勉強中!!</h2>
+      <div className="text-center mt-10 sm:mt-20 mb-10 sm:mb-20 w-full font-bold text-gray-800 relative z-10">
+        <h1 className="text-4xl sm:text-6xl">
+          Okki-です!!<span>これからよろしく!!</span>
+        </h1>
+        <h2 className="text-2xl sm:text-4xl mt-4">Python&React勉強中!!</h2>
       </div>
 
       {/* Link */}
-      <div className="group flex flex-col items-center mb-20 w-full relative">
-        <div className="relative w-full max-w-full h-[300px] overflow-hidden">
+      <div className="group flex flex-col items-center mb-10 sm:mb-20 w-full relative">
+        <div className="relative w-full max-w-full h-[200px] sm:h-[300px] overflow-hidden">
           <Image
             src={SampleSVG}
             alt="React Logo"
@@ -97,12 +99,12 @@ export default function Home() {
             className="absolute inset-0 opacity-0 transition-all duration-500 transform translate-x-full translate-y-full group-hover:opacity-100 group-hover:translate-x-0 group-hover:translate-y-0"
           />
           <div className="flex flex-col items-center w-full relative">
-            <h3 className="text-5xl text-center font-bold text-gray-800 relative z-10">
+            <h3 className="text-3xl sm:text-5xl text-center font-bold text-gray-800 relative z-10">
               My Account
             </h3>
-            <div className="mt-8 text-4xl w-1/2 relative z-10">
+            <div className="mt-4 sm:mt-8 text-2xl sm:text-4xl w-3/4 sm:w-1/2 relative z-10">
               <Link
-                className="group/link border-solid border-2 p-4 rounded-lg border-gray-300 hover:border-blue-500 flex transition-all hover:bg-blue-50 hover:shadow-lg"
+                className="group/link border-solid border-2 p-2 sm:p-4 rounded-lg border-gray-300 hover:border-blue-500 flex transition-all hover:bg-blue-50 hover:shadow-lg"
                 href={`https://x.com/${xHandle}`}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -110,16 +112,16 @@ export default function Home() {
                 <Image
                   src="/images/x_logo.jpg"
                   alt="x_logo"
-                  width={50}
-                  height={50}
-                  className="mr-4 opacity-0 group-hover/link:opacity-100 transition-opacity duration-300"
+                  width={30}
+                  height={30}
+                  className="mr-2 sm:mr-4 opacity-0 group-hover/link:opacity-100 transition-opacity duration-300"
                 />
                 <p className="font-semibold text-gray-700 group-hover/link:text-blue-500 transition-colors duration-300">
                   X &rarr;
                 </p>
               </Link>
               <Link
-                className="group/link border-solid border-2 p-4 rounded-lg border-gray-300 hover:border-blue-500 flex transition-all hover:bg-blue-50 hover:shadow-lg"
+                className="group/link border-solid border-2 p-2 sm:p-4 rounded-lg border-gray-300 hover:border-blue-500 flex transition-all hover:bg-blue-50 hover:shadow-lg"
                 href={`https://github.com/${githubHandle}`}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -127,9 +129,9 @@ export default function Home() {
                 <Image
                   src="/images/github_logo.jpg"
                   alt="github_logo"
-                  width={50}
-                  height={50}
-                  className="mr-4 opacity-0 group-hover/link:opacity-100 transition-opacity duration-300"
+                  width={30}
+                  height={30}
+                  className="mr-2 sm:mr-4 opacity-0 group-hover/link:opacity-100 transition-opacity duration-300"
                 />
                 <p className="font-semibold text-gray-700 group-hover/link:text-blue-500 transition-colors duration-300">
                   GitHub &rarr;
@@ -141,19 +143,19 @@ export default function Home() {
       </div>
 
       {/* profile */}
-      <div className="group text-center mb-20 w-full relative">
+      <div className="group text-center mb-10 sm:mb-20 w-full relative">
         <div
           className="
-        absolute 
-        inset-0 
-        opacity-0
-        transform 
-        scale-100
-        transition-all 
-        duration-500 
-        group-hover:opacity-100 
-        group-hover:scale-150
-      "
+          absolute 
+          inset-0 
+          opacity-0
+          transform 
+          scale-100
+          transition-all 
+          duration-500 
+          group-hover:opacity-100 
+          group-hover:scale-150
+        "
         >
           <Image
             src={CircleSVG}
@@ -162,7 +164,7 @@ export default function Home() {
             objectFit="contain"
           />
         </div>
-        <h3 className="text-5xl font-bold text-gray-800 relative z-10">
+        <h3 className="text-3xl sm:text-5xl font-bold text-gray-800 relative z-10">
           profile
         </h3>
         <div className="mt-3 relative z-10">
@@ -170,9 +172,15 @@ export default function Home() {
             modules={[Navigation, Pagination]}
             loop={false}
             speed={1500}
-            slidesPerView={2} // Adjust this to show part of the next slide
-            centeredSlides={true} // Center the slide when it comes into view
-            spaceBetween={30}
+            slidesPerView={1} 
+            centeredSlides={true}
+            spaceBetween={10} 
+            breakpoints={{
+              640: {
+                slidesPerView: 2, 
+                spaceBetween: 30,
+              },
+            }}
             navigation={{
               nextEl: ".swiper-button-next",
               prevEl: ".swiper-button-prev",
